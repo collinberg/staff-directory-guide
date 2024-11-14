ab726f4121452db7b34484831c573e69bd0fb0fa
Collin Berg <berg.collin@gmail.com>
2024-11-13 21:30:58 -0500

    Added Staff Profile Block

--- src/edit.js
+++ src/edit.js
@@ -1,74 +1,56 @@
-/**
- * Retrieves the translation of text.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-i18n/
- */
-import { __ } from '@wordpress/i18n';
+import { useBlockProps } from '@wordpress/block-editor';
 
-/**
- * React hook that is used to mark the block wrapper element.
- * It provides all the necessary props like the class name.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
- */
-import { InspectorControls, useBlockProps } from '@wordpress/block-editor';
+import { useSelect } from '@wordpress/data';
+import { useEntityProp } from '@wordpress/core-data';
 
-
-import { PanelBody, TextControl, ToggleControl } from '@wordpress/components';
+// NEW IMPORTS
+import { RangeControl, __experimentalInputControl as InputControl } from '@wordpress/components';
+// Technically the input control is flagged experimental, but I've heard this will be stable quite soon - we're going to use it either way, but you could just create your own input component if you prefer
 
 
 
-/**
- * The edit function describes the structure of your block in the context of the
- * editor. This represents what the editor will render when the block is used.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#edit
- *
- * @return {Element} Element to render.
- */
-export default function Edit( { attributes, setAttributes } ) {
-    const { showStartingYear, startingYear } = attributes;
-    const currentYear = new Date().getFullYear().toString();
+import './editor.scss';
 
-    let displayDate;
+export default function Edit() {
 
-    if ( showStartingYear && startingYear ) {
-            displayDate = startingYear + '–' + currentYear;
-    } else {
-        displayDate = currentYear;
-    }
+	// Attributes for the block wrapper
+	const blockProps = useBlockProps()
 
-    return (
-        <>
-            <InspectorControls>
-                <PanelBody title={ __( 'Settings', 'copyright-date-block' ) }>
-                    <ToggleControl
-                        checked={ !! showStartingYear }
-                        label={ __(
-                            'Show starting year',
-                            'copyright-date-block'
-                        ) }
-                        onChange={ () =>
-                            setAttributes( {
-                                showStartingYear: ! showStartingYear,
-                            } )
-                        }
-                    />
-                    { showStartingYear && (
-                        <TextControl
-                            label={ __(
-                                'Starting year',
-                                'copyright-date-block'
-                            ) }
-                            value={ startingYear || '' }
-                            onChange={ ( value ) =>
-                                setAttributes( { startingYear: value } )
-                            }
-                        />
-                    ) }
-                </PanelBody>
-            </InspectorControls>
-            <p { ...useBlockProps() }>© { displayDate }</p>
-        </>
-    );
+	// Determine the curent post type in the editor context
+	const currentPostType =  useSelect((select) => {
+		return select('core/editor').getCurrentPostType()
+	}, [])
+
+	// Fetch the meta as an object and the setMeta function
+	const [meta, setMeta] = useEntityProp('postType', currentPostType, 'meta');
+
+	// Destructure all our individual properties from the meta for ease of use
+	const {employeeTitle, employeeEmail} = meta
+
+	// Flexible helper for setting a single meta value w/o mutating state
+	const updateMeta = ( key, value ) => {
+		setMeta( { ...meta, [key]: value } );
+	};
+
+	return (
+		<div { ...blockProps }>
+			<h4>Staff Details:</h4>
+			<hr />
+			<div className="fsd-property-meta-details__grid">
+				<InputControl
+					type="string"
+					label='Employee Title'
+					value={ employeeTitle || '' }
+					onChange={ ( value ) => updateMeta( "employeeTitle",value  )}
+                />
+				<InputControl
+					type="string"
+					label='Employee Email'
+					value={ employeeEmail || '' }
+					onChange={ ( value ) => updateMeta( "employeeEmail",value  )}
+                />
+			</div>
+
+		</div>
+	);
 }
