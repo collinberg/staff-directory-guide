20524e70251202ecc242a1b7241a2e856b99b5ea
Collin Berg <berg.collin@gmail.com>
2024-11-14 18:25:40 -0500

    Added in Shortcode, and template over ride again

--- staff.php
+++ staff.php
@@ -36,8 +36,8 @@ register_activation_hook( __FILE__, array( $post_type, 'activate' ) );
 $post_type_registrations->init();
 
 // Initialize metaboxes
-//$post_type_metaboxes = new Staff_Post_Type_Metaboxes;
-//$post_type_metaboxes->init();
+$post_type_metaboxes = new Staff_Post_Type_Metaboxes;
+$post_type_metaboxes->init();
 
 /**
  * Registers the CSS file for the archive and shortcode.
@@ -46,11 +46,9 @@ $post_type_registrations->init();
  * @see https://developer.wordpress.org/reference/hooks/wp_enqueue_scripts/
  */
 function staff_styles(){
-  if( !wp_is_block_theme() ):
     if( is_page() && !is_front_page() ) {
       wp_enqueue_style( 'staff-styles', plugin_dir_url(__FILE__) .'assets/css/staff.css' );
     }
-  endif;
 }
 add_action('wp_enqueue_scripts','staff_styles');
 /**
@@ -68,72 +66,75 @@ add_action('init', 'staff_register_meta_block');
 
 
 
-// add_filter( 'upload_mimes', 'my_myme_types', 1, 1 );
-// function my_myme_types( $mime_types ) {
-//   $mime_types['vcf'] = 'text/x-vcard'; // Adding .json extension
+add_filter( 'upload_mimes', 'my_myme_types', 1, 1 );
+function my_myme_types( $mime_types ) {
+  $mime_types['vcf'] = 'text/x-vcard'; // Adding .json extension
   
-//   return $mime_types;
-// }
+  return $mime_types;
+}
 
-// Add Shortcode
-// function staff_shortcode() {
+/**
+ *
+ * Register a Shortcode for displaying a list of hte staff
+ */
+function staff_shortcode() {
 
-//   $args = [
-//     'post_type'       => 'staff',
-//     'posts_per_page'  => -1,
-//     'orderby' => 'menu_order', 
-//     'order' => 'ASC',
-//   ];
+  $args = [
+    'post_type'       => 'staff',
+    'posts_per_page'  => -1,
+    'orderby' => 'menu_order', 
+    'order' => 'ASC',
+  ];
 
-// 	// Query
-// 	$the_query = new WP_Query( $args );
+	// Query
+	$the_query = new WP_Query( $args );
 	
-// 	// Posts
-// 	$output = '<ul class="staff-list">';
-// 	while ( $the_query->have_posts() ) :
+	// Posts
+	$output = '<ul class="staff-list">';
+	while ( $the_query->have_posts() ) :
 	
-// 		$the_query->the_post();
+		$the_query->the_post();
 		
-// 		if( "" != get_the_post_thumbnail_url( $the_query->ID, 'thumbnail') ):
-// 			$post_thumb = get_the_post_thumbnail_url( $the_query->ID, 'full');
-// 		else:
-// 			$post_thumb = plugins_url('assets/img/profile-placeholder.jpg', __FILE__);
-// 		endif;
+		if( "" != get_the_post_thumbnail_url( $the_query->ID, 'thumbnail') ):
+			$post_thumb = get_the_post_thumbnail_url( $the_query->ID, 'full');
+		else:
+			$post_thumb = plugins_url('assets/img/profile-placeholder.jpg', __FILE__);
+		endif;
 		
-// 		$output .=  '<li>';
+		$output .=  '<li>';
 
-//       $output .=  '<a href="' . get_permalink() . '" class="profileLink" title="' . get_the_title() . '">';
-//       $output .=  '<img src="' . $post_thumb . '"/>';
-//       $output .=  '<div class="hover-area"><div class="hoverbg"></div><span>→</span></div></a>';
-//       $output .=  '<h3>' . get_the_title() . '</h3>';
-//       $output .=  '<span class="staff-title-sc">' . get_post_meta( get_the_ID(), 'profile_title',  true ) . '</span>';
-//     $output .=  '</li>';
+      $output .=  '<a href="' . get_permalink() . '" class="profileLink" title="' . get_the_title() . '">';
+      $output .=  '<img src="' . $post_thumb . '"/>';
+      $output .=  '<div class="hover-area"><div class="hoverbg"></div><span>→</span></div></a>';
+      $output .=  '<h3>' . get_the_title() . '</h3>';
+      $output .=  '<span class="staff-title-sc">' . get_post_meta( get_the_ID(), 'profile_title',  true ) . '</span>';
+    $output .=  '</li>';
 		
-// 	endwhile;
-// 	$output .= '</ul>';
+	endwhile;
+	$output .= '</ul>';
 	
-// 	// Reset post data
-// 	wp_reset_postdata();
+	// Reset post data
+	wp_reset_postdata();
 	
-// 	// Return code
-// 	return $output;
+	// Return code
+	return $output;
 
-// }
-// add_shortcode( 'staff', 'staff_shortcode' );
+}
+add_shortcode( 'staff', 'staff_shortcode' );
 
-// function staff_single_template($single) {
-//     global $post;
+function staff_single_template($single) {
+    global $post;
 
-//     // Check if the post type matches
-//     if ($post->post_type == 'staff' && !wp_is_block_theme() ) {
-//         // Locate the custom template in your plugin folder
-//         if (file_exists(plugin_dir_path(__FILE__) . '/templates/single-staff.php')) {
-//             return plugin_dir_path(__FILE__) . '/templates/single-staff.php';
-//         }
-//     }
-//     return $single;
-// }
-// add_filter('single_template', 'staff_single_template');
+    // Check if the post type matches
+    if ($post->post_type == 'staff' && !wp_is_block_theme() ) {
+        // Locate the custom template in your plugin folder
+        if (file_exists(plugin_dir_path(__FILE__) . '/templates/single-staff.php')) {
+            return plugin_dir_path(__FILE__) . '/templates/single-staff.php';
+        }
+    }
+    return $single;
+}
+add_filter('single_template', 'staff_single_template');
 
 
 
